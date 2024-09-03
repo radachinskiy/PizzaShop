a3eb35af56c52cfc9c389a26468684c39db31682
Vasiliy R <ya-kubanec@yandex.ru>
2024-09-03 15:40:01 +0300

    Create product migration

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:pizzashop.db"
+#set :database, "sqlite3:pizzashop.db"
+set :database, {adapter: "sqlite3", database: "pizzashop.db"}
 
 class Product < ActiveRecord::Base
 end
