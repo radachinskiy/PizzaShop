07e9ff444d29286ec75445c58141be83b21f66b3
Vasiliy R <ya-kubanec@yandex.ru>
2024-09-06 15:42:43 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -38,3 +38,21 @@ function something()
 
     return cnt;
   }
+
+  function cart_get_orders()
+  {
+    var orders = '';
+
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+      var key = window.localStorage.key(i); // получаем ключ
+      var value = window.localStorage.getItem(key); // получаем значение
+
+      if(key.indexOf('product_') == 0)
+      {
+        orders = orders + key + '=' + value + ',';
+      }
+    } 
+
+    return orders ;
+  }
